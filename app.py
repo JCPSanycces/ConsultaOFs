fb05d5c9810420709daa9d0fb399be7c4fb2a7be
JCPSanycces <jl.chiva@sanycces.es>
2026-07-02 15:19:07 +0200

    Cambio en la IP de la impresora

--- app.py
+++ app.py
@@ -364,7 +364,7 @@ def imprimir_etiqueta():
     zpl_bytes = zpl.encode('utf-8')
 
     impresoras = [
-        {'nombre': 'Zebra ZT230',  'ip': '192.168.1.981', 'puerto': 9100},
+        {'nombre': 'Zebra ZT230',  'ip': '192.168.1.81', 'puerto': 9100},
         # {'nombre': 'Godex G500',   'ip': '192.168.1.87', 'puerto': 9100},
     ]
 
